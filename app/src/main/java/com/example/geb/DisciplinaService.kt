86fe81107b2664e96004c70b1a0432b640bfb2b3
Daniel Ribeiro <42393128+ribeirosilva@users.noreply.github.com>
2019-10-20 22:09:46 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/geb/DisciplinaService.kt
@@ -0,0 +1,43 @@
+package com.example.geb
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+object DisciplinaService {
+
+    //TROQUE PELA URL DE ONDE ESTÁ O WS
+
+    val host = "https://github.com/ribeirosilva/G.E.B.-Android.git"
+    val TAG = "WS_LMSApp"
+
+    fun getDisciplinas (context: Context): List<Disciplina> {
+        if (AndroidUtils.isInternetDisponivel(context)) {
+            val url = "$host/disciplinas"
+            val json = HttpHelper.get(url)
+            return parserJson(json)
+        } else {
+            return ArrayList<Disciplina>()
+        }
+    }
+
+    fun save(disciplina: Disciplina): Response {
+        val json = HttpHelper.post("$host/disciplinas", disciplina.toJson())
+        return parserJson(json)
+    }
+
+    fun delete(disciplina: Disciplina): Response {
+        Log.d(TAG, disciplina.id.toString())
+        val url = "$host/disciplinas/${disciplina.id}"
+        val json = HttpHelper.delete(url)
+        Log.d(TAG, json)
+        return parserJson(json)
+    }
+
+    inline fun <reified T> parserJson(json: String): T {
+        val type = object : TypeToken<T>(){}.type
+        return Gson().fromJson<T>(json, type)
+    }
+}
